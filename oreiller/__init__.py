4e90f181b8fd2d0728db2065f88e64397a000b08
Abdur-RahmaanJ <cryptolabour@gmail.com>
2023-12-27 07:04:29 +0400

    feat: line

--- oreiller/__init__.py
+++ oreiller/__init__.py
@@ -1,3 +1,5 @@
 from .oreiller import Oreiller
 
+
+
 __version__ = "0.0.1"
--- oreiller/oreiller.py
+++ oreiller/oreiller.py
@@ -1,6 +1,8 @@
 from PIL import Image
+from PIL import ImageDraw
 
 class Oreiller:
+    image = None
 
     @classmethod
     def open(cls, *args, **kwargs):
@@ -9,3 +11,12 @@ class Oreiller:
     @classmethod
     def new(cls, *args, **kwargs):
         return Image.new(*args, **kwargs)
+    
+    @classmethod
+    def line(cls, *args, **kwargs):
+        if cls.image is None:
+            raise Exception('Please open or set an image')
+        draw = ImageDraw.Draw(cls.image)
+        draw.line(*args, **kwargs)
+
+    
