be124d380d88575fc6e6f8a36bd85ac24d223ef2
Abdur-RahmaanJ <cryptolabour@gmail.com>
2023-12-28 12:36:36 +0400

    feat: fill() impact all functions

--- oreiller/oreiller.py
+++ oreiller/oreiller.py
@@ -37,6 +37,9 @@ class Oreiller:
     def line(cls, *args, **kwargs):
         if cls.image is None:
             raise Exception('Please open or set an image')
+        if cls._fill is not None:
+            if 'fill' not in kwargs:
+                kwargs['fill'] = cls._fill
         draw = ImageDraw.Draw(cls.image)
         draw.line(*args, **kwargs)
 
@@ -45,14 +48,20 @@ class Oreiller:
     def oline(cls, x1, y1, x2, y2, **kwargs):
         if cls.image is None:
             raise Exception('Please open or set an image')
+        if cls._fill is not None:
+            if 'fill' not in kwargs:
+                kwargs['fill'] = cls._fill
         draw = ImageDraw.Draw(cls.image)
         shape = [(x1, y1), (x2, y2)]
-        draw.line(shape, fill=cls._fill, **kwargs)
+        draw.line(shape, **kwargs)
 
     @classmethod
     def otext(cls, x, y, text, **kwargs):
         if cls.image is None:
             raise Exception('Please open or set an image')
+        if cls._fill is not None:
+            if 'fill' not in kwargs:
+                kwargs['fill'] = cls._fill
         with Pilmoji(cls.image) as pilmoji:
             pilmoji.text((x, y), text, cls._fill, cls._font, **kwargs)
 
@@ -60,6 +69,10 @@ class Oreiller:
     def arc(cls, *args, **kwargs):
         if cls.image is None:
             raise Exception('Please open or set an image')
+        if cls._fill is not None:
+            if 'fill' not in kwargs:
+                kwargs['fill'] = cls._fill
+        
         img1 = ImageDraw.Draw(cls.image)   
         img1.arc(*args, **kwargs) 
 
@@ -67,6 +80,10 @@ class Oreiller:
     def oarc(cls, x1, y1, x2, y2, start, end, **kwargs):
         if cls.image is None:
             raise Exception('Please open or set an image')
+        if cls._fill is not None:
+            if 'fill' not in kwargs:
+                kwargs['fill'] = cls._fill
+        
         img1 = ImageDraw.Draw(cls.image) 
         shape = [(x1, y1), (x2, y2)] 
         img1.arc(shape, start, end, **kwargs) 
@@ -76,6 +93,10 @@ class Oreiller:
     def chord(cls, *args, **kwargs):
         if cls.image is None:
             raise Exception('Please open or set an image')
+        if cls._fill is not None:
+            if 'fill' not in kwargs:
+                kwargs['fill'] = cls._fill
+        
         img1 = ImageDraw.Draw(cls.image)   
         img1.chord(*args, **kwargs) 
 
@@ -83,6 +104,10 @@ class Oreiller:
     def ochord(cls, x1, y1, x2, y2, start, end, **kwargs):
         if cls.image is None:
             raise Exception('Please open or set an image')
+        if cls._fill is not None:
+            if 'fill' not in kwargs:
+                kwargs['fill'] = cls._fill
+
         img1 = ImageDraw.Draw(cls.image) 
         shape = [(x1, y1), (x2, y2)] 
         img1.chord(shape, start, end, **kwargs) 
